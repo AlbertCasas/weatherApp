501b8bf24c029ad1ddc0b27632e19d15c4bd1aa6
Albert Casas <albertcasas3d@gmail.com>
2022-09-15 18:42:47 +0200

    adjusting env

--- src/firebase/firebaseinit.js
+++ src/firebase/firebaseinit.js
@@ -5,7 +5,7 @@ import 'firebase/compat/firestore';
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
+    projectId: "weatherapp-a7654",
     storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: process.env.MESSAGIG_SENDER_ID,
     appId: process.env.APP_ID
